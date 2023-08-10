8b48a2e985f3e9fcaee37ccdddad6fc5b89b6f72
lasseclaes <lasseclaes@gmail.com>
2023-08-10 10:53:24 +0200

    +amend

---
+++ src/stores/examorders.js
@@ -0,0 +1,450 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useExamOrdersStore = defineStore('examOrdersStore', () => {
+  const examOrders = [
+    {
+      id: 11175,
+      examcode: 'Q_EXAM_2365',
+      examdate: new Intl.DateTimeFormat('da').format(
+        new Date(Date.UTC(2023, 11, 20, 3, 23, 16, 738))
+      ),
+      examname: 'Musik A',
+      docformat: 'pdf',
+      visionimpaired_exam: false,
+      itemid: 1,
+      status: 'Ubehandlede',
+      special_production: false,
+      att: 'ljb'
+    },
+    {
+      id: 11176,
+      examcode: 'Q_EXAM_236sd5',
+      examdate: new Intl.DateTimeFormat('da').format(
+        new Date(Date.UTC(2023, 11, 20, 1, 23, 16, 738))
+      ),
+      examname: 'Musik B',
+      docformat: 'docx_rtf',
+      visionimpaired_exam: true,
+      itemid: 2,
+      status: 'Korrektur',
+      special_production: true,
+      att: 'per'
+    },
+    {
+      id: 11177,
+      examcode: 'Q_EXAM_2365',
+      examdate: new Intl.DateTimeFormat('da').format(
+        new Date(Date.UTC(2023, 11, 20, 3, 23, 16, 738))
+      ),
+      examname: 'Musik C',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 56326,
+      status: 'Ubehandlede',
+      special_production: false,
+      att: ''
+    },
+    {
+      id: 11178,
+      examcode: 'Q_EXAM_2365',
+      examdate: new Intl.DateTimeFormat('da').format(
+        new Date(Date.UTC(2023, 11, 20, 3, 23, 16, 738))
+      ),
+      examname: 'Musik D',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 56326,
+      status: 'Godkendte',
+      special_production: false,
+      att: ''
+    },
+    {
+      id: 11179,
+      examcode: 'Q_EXAM_2365',
+      examdate: new Intl.DateTimeFormat('da').format(
+        new Date(Date.UTC(2023, 11, 20, 3, 23, 16, 738))
+      ),
+      examname: 'Musik A',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 56326,
+      status: 'Direkte-forsendelse',
+      special_production: true,
+      att: ''
+    },
+    {
+      id: 11180,
+      examcode: 'Q_EXAM_2365',
+      examdate: new Intl.DateTimeFormat('da').format(
+        new Date(Date.UTC(2023, 11, 20, 3, 23, 16, 738))
+      ),
+      examname: 'Musik A',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 56326,
+      status: 'Dialog',
+      special_production: false,
+      att: ''
+    },
+    {
+      id: 11181,
+      examcode: 'Q_EXAM_2365',
+      examdate: new Intl.DateTimeFormat('da').format(
+        new Date(Date.UTC(2023, 11, 20, 3, 23, 16, 738))
+      ),
+      examname: 'Musik A',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 56326,
+      status: 'Dialog',
+      special_production: true,
+      att: 'Keld'
+    },
+    {
+      id: 11182,
+      examcode: 'Q_EXAM_2365',
+      examdate: new Intl.DateTimeFormat('da').format(
+        new Date(Date.UTC(2023, 11, 20, 3, 23, 16, 738))
+      ),
+      examname: 'Musik A',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 56326,
+      status: 'Dialog',
+      special_production: true,
+      att: ''
+    },
+    {
+      id: 11183,
+      examcode: 'Q_EXAM_2365',
+      examdate: new Intl.DateTimeFormat('da').format(
+        new Date(Date.UTC(2023, 11, 20, 3, 23, 16, 738))
+      ),
+      examname: 'Musik A',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 56326,
+      status: 'Ubehandlede',
+      special_production: true,
+      att: ''
+    },
+    {
+      id: 11184,
+      examcode: 'Q_EXAM_2365',
+      examdate: new Intl.DateTimeFormat('da').format(
+        new Date(Date.UTC(2023, 11, 20, 3, 23, 16, 738))
+      ),
+      examname: 'Musik A',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 56326,
+      status: 'Ubehandlede',
+      special_production: true,
+      att: ''
+    },
+    {
+      id: 11185,
+      examcode: 'Q_EXAM_1234',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 1, 10, 30, 0, 0))),
+      examname: 'Science',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 3,
+      status: 'Godkendte',
+      special_production: false,
+      att: 'john'
+    },
+    {
+      id: 11186,
+      examcode: 'Q_EXAM_5678',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 2, 14, 15, 0, 0))),
+      examname: 'History',
+      docformat: 'docx_rtf',
+      visionimpaired_exam: false,
+      itemid: 4,
+      status: 'Korrektur',
+      special_production: true,
+      att: 'susan'
+    },
+    {
+      id: 11187,
+      examcode: 'Q_EXAM_9101',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 3, 9, 0, 0, 0))),
+      examname: 'Mathematics',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 5,
+      status: 'Ubehandlede',
+      special_production: false,
+      att: 'michael'
+    },
+    {
+      id: 11188,
+      examcode: 'Q_EXAM_1122',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 4, 11, 45, 0, 0))),
+      examname: 'Language Arts',
+      docformat: 'pdf',
+      visionimpaired_exam: false,
+      itemid: 6,
+      status: 'Direkte-forsendelse',
+      special_production: true,
+      att: 'emily'
+    },
+    {
+      id: 11189,
+      examcode: 'Q_EXAM_3344',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 5, 13, 20, 0, 0))),
+      examname: 'Geography',
+      docformat: 'docx_rtf',
+      visionimpaired_exam: false,
+      itemid: 7,
+      status: 'Dialog',
+      special_production: false,
+      att: 'david'
+    },
+    {
+      id: 11190,
+      examcode: 'Q_EXAM_5566',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 6, 16, 45, 0, 0))),
+      examname: 'Physical Education',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 8,
+      status: 'Korrektur',
+      special_production: true,
+      att: 'jessica'
+    },
+    {
+      id: 11191,
+      examcode: 'Q_EXAM_7788',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 7, 10, 0, 0, 0))),
+      examname: 'Art',
+      docformat: 'pdf',
+      visionimpaired_exam: false,
+      itemid: 9,
+      status: 'Ubehandlede',
+      special_production: false,
+      att: 'andrew'
+    },
+    {
+      id: 11192,
+      examcode: 'Q_EXAM_9900',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 8, 14, 30, 0, 0))),
+      examname: 'Computer Science',
+      docformat: 'docx_rtf',
+      visionimpaired_exam: true,
+      itemid: 10,
+      status: 'Godkendte',
+      special_production: false,
+      att: 'natalie'
+    },
+    {
+      id: 11193,
+      examcode: 'Q_EXAM_1122',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 9, 11, 15, 0, 0))),
+      examname: 'Biology',
+      docformat: 'pdf',
+      visionimpaired_exam: false,
+      itemid: 11,
+      status: 'Dialog',
+      special_production: true,
+      att: 'matthew'
+    },
+    {
+      id: 11194,
+      examcode: 'Q_EXAM_3344',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 10, 12, 0, 0, 0))),
+      examname: 'Chemistry',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 12,
+      status: 'Ubehandlede',
+      special_production: true,
+      att: 'olivia'
+    },
+    {
+      id: 11195,
+      examcode: 'Q_EXAM_5566',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 11, 9, 30, 0, 0))),
+      examname: 'Foreign Language',
+      docformat: 'docx_rtf',
+      visionimpaired_exam: false,
+      itemid: 13,
+      status: 'Direkte-forsendelse',
+      special_production: false,
+      att: 'william'
+    },
+    {
+      id: 11196,
+      examcode: 'Q_EXAM_7788',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 12, 15, 15, 0, 0))),
+      examname: 'Economics',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 14,
+      status: 'Korrektur',
+      special_production: true,
+      att: 'emma'
+    },
+    {
+      id: 11197,
+      examcode: 'Q_EXAM_9900',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 13, 10, 45, 0, 0))),
+      examname: 'Physical Science',
+      docformat: 'pdf',
+      visionimpaired_exam: false,
+      itemid: 15,
+      status: 'Godkendte',
+      special_production: false,
+      att: 'jacob'
+    },
+    {
+      id: 11198,
+      examcode: 'Q_EXAM_1122',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 14, 13, 0, 0, 0))),
+      examname: 'Literature',
+      docformat: 'docx_rtf',
+      visionimpaired_exam: true,
+      itemid: 16,
+      status: 'Ubehandlede',
+      special_production: true,
+      att: 'sophia'
+    },
+    {
+      id: 11199,
+      examcode: 'Q_EXAM_3344',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 15, 11, 30, 0, 0))),
+      examname: 'Social Studies',
+      docformat: 'pdf',
+      visionimpaired_exam: false,
+      itemid: 17,
+      status: 'Dialog',
+      special_production: false,
+      att: 'ethan'
+    },
+    {
+      id: 11200,
+      examcode: 'Q_EXAM_5566',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 16, 14, 45, 0, 0))),
+      examname: 'History',
+      docformat: 'pdf',
+      visionimpaired_exam: false,
+      itemid: 18,
+      status: 'Ubehandlede',
+      special_production: false,
+      att: 'ava'
+    },
+    {
+      id: 11201,
+      examcode: 'Q_EXAM_7788',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 17, 10, 0, 0, 0))),
+      examname: 'Geometry',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 19,
+      status: 'Godkendte',
+      special_production: true,
+      att: 'noah'
+    },
+    {
+      id: 11202,
+      examcode: 'Q_EXAM_9900',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 18, 15, 30, 0, 0))),
+      examname: 'Physics',
+      docformat: 'docx_rtf',
+      visionimpaired_exam: true,
+      itemid: 20,
+      status: 'Dialog',
+      special_production: false,
+      att: 'mia'
+    },
+    {
+      id: 11203,
+      examcode: 'Q_EXAM_1122',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 19, 11, 15, 0, 0))),
+      examname: 'Health',
+      docformat: 'pdf',
+      visionimpaired_exam: false,
+      itemid: 21,
+      status: 'Korrektur',
+      special_production: true,
+      att: 'william'
+    },
+    {
+      id: 11204,
+      examcode: 'Q_EXAM_3344',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 20, 12, 0, 0, 0))),
+      examname: 'Psychology',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 22,
+      status: 'Godkendte',
+      special_production: true,
+      att: 'olivia'
+    },
+    {
+      id: 11205,
+      examcode: 'Q_EXAM_5566',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 21, 9, 30, 0, 0))),
+      examname: 'Sociology',
+      docformat: 'docx_rtf',
+      visionimpaired_exam: false,
+      itemid: 23,
+      status: 'Ubehandlede',
+      special_production: false,
+      att: 'james'
+    },
+    {
+      id: 11206,
+      examcode: 'Q_EXAM_7788',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 22, 15, 15, 0, 0))),
+      examname: 'Geography',
+      docformat: 'pdf',
+      visionimpaired_exam: true,
+      itemid: 24,
+      status: 'Dialog',
+      special_production: true,
+      att: 'emily'
+    },
+    {
+      id: 11207,
+      examcode: 'Q_EXAM_9900',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 23, 10, 45, 0, 0))),
+      examname: 'Political Science',
+      docformat: 'pdf',
+      visionimpaired_exam: false,
+      itemid: 25,
+      status: 'Korrektur',
+      special_production: false,
+      att: 'liam'
+    },
+    {
+      id: 11208,
+      examcode: 'Q_EXAM_1122',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 24, 13, 0, 0, 0))),
+      examname: 'Philosophy',
+      docformat: 'docx_rtf',
+      visionimpaired_exam: true,
+      itemid: 26,
+      status: 'Godkendte',
+      special_production: true,
+      att: 'ava'
+    },
+    {
+      id: 11209,
+      examcode: 'Q_EXAM_3344',
+      examdate: new Intl.DateTimeFormat('da').format(new Date(Date.UTC(2023, 8, 25, 11, 30, 0, 0))),
+      examname: 'Environmental Science',
+      docformat: 'pdf',
+      visionimpaired_exam: false,
+      itemid: 27,
+      status: 'Ubehandlede',
+      special_production: false,
+      att: 'noah'
+    }
+  ]
+  return {
+    examOrders: ref(examOrders)
+  }
+})
