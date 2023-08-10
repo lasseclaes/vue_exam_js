64985738c55d843915a97151ab1d9445808d83b6
lasseclaes <lasseclaes@gmail.com>
2023-08-10 14:39:45 +0200

    Table options

--- src/views/BestillingerView.vue
+++ src/views/BestillingerView.vue
@@ -18,12 +18,24 @@ console.log(examOrdersLoc.examOrders)
 
 <template>
   <div class="card bestillinger">
-    <DataTable :value="examOrdersLoc.examOrders" tableStyle="min-width: 50rem">
+    <DataTable
+      :value="examOrdersLoc.examOrders"
+      tableStyle="min-width: 50rem"
+      showGridlines
+      paginator
+      :rows="5"
+      :rowsPerPageOptions="[5, 10, 20, 50]"
+      paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+      currentPageReportTemplate="{first} to {last} of {totalRecords}"
+      removableSort
+    >
       <Column
         v-for="col in columns"
         :key="col.field"
         :field="col.field"
         :header="col.header"
+        style="width: fit-content"
+        sortable
       ></Column>
     </DataTable>
   </div>
