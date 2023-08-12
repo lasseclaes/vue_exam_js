c3959f44d10ce520497f023a6e34675747fe497d
lasseclaes <lasseclaes@gmail.com>
2023-08-12 12:35:40 +0200

    URL params

--- src/views/NothingHereView.vue
+++ src/views/NothingHereView.vue
@@ -1,4 +1,14 @@
-<script lang="ts"></script>
+<!-- <script setup lang="ts">
+import { URL } from 'url' -->
+<script setup>
+const url = new URL(window.location)
+const param = url.searchParams.get('test')
+
+console.log('param:', param)
+console.log(window.location.pathname)
+const cleanPathname = window.location.pathname.slice(1)
+console.log('cleanPathname:', cleanPathname)
+</script>
 <template>
   <h5>Nothing here</h5>
 </template>
