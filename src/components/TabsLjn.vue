c6f7415bf8d017eff19c091a8d908c43eca64bba
lasseclaes <lasseclaes@gmail.com>
2023-08-15 10:51:27 +0200

    using Vue Router instead of native JS

--- src/components/TabsLjn.vue
+++ src/components/TabsLjn.vue
@@ -1,59 +1,68 @@
 <script setup>
 import TabMenu from 'primevue/tabmenu'
-import { ref } from 'vue'
+import { ref, onUpdated } from 'vue'
+import { useRoute } from 'vue-router'
+const items = ref([
+  {
+    label: 'Ubehandlede',
+    to: { path: '/bestillinger', query: { status: 'ubehandlede' } }
+  },
+  {
+    label: 'Dialog',
+    to: { path: '/bestillinger', query: { status: 'dialog' } }
+  },
+  {
+    label: 'Korrektur',
+    to: { path: '/bestillinger', query: { status: 'korrektur' } }
+  },
+  {
+    label: 'Godkendte',
+    to: { path: '/bestillinger', query: { status: 'godkendte' } }
+  },
+  {
+    label: 'Til Brænding',
+    to: { path: '/bestillinger', query: { status: 'tilbraending' } }
+  },
+  {
+    label: 'Brændt',
+    to: { path: '/bestillinger', query: { status: 'braendte' } }
+  },
+  {
+    label: 'Kontrolleret',
+    to: { path: '/bestillinger', query: { status: 'kontrolleret' } }
+  },
+  {
+    label: 'Direkte forsendelse',
+    to: { path: '/bestillinger', query: { status: 'direkteforsendelse' } }
+  }
+])
+
+onUpdated(() => {
+  console.log('HEY onUpdated')
+})
+
+const $route = useRoute()
 
 function onTabChange(e) {
   console.log('onTabChange:', e)
-  const url = new URL(window.location)
+  console.log($route.query.status)
+
+  //const url = new URL(window.location)
   // const param = url.searchParams.get('test')
   // console.log('param:', param)
-  const status_filter = url.searchParams.get('status')
-  console.log('status_filter:', status_filter)
+  //const status_filter = url.searchParams.get('status')
+  //console.log('status_filter:', status_filter)
+
   /*   console.log(window.location.pathname)
   const cleanPathname = window.location.pathname.slice(1)
   console.log('cleanPathname:', cleanPathname) */
 }
-
-const items = ref([
-  {
-    label: 'Ubehandlede',
-    to: '/bestillinger?status=ubehandlede'
-  },
-  {
-    label: 'Dialog',
-    to: '/bestillinger?status=dialog'
-  },
-  {
-    label: 'Korrektur',
-    to: '/bestillinger?status=korrektur'
-  },
-  {
-    label: 'Godkendte',
-    to: '/bestillinger?status=godkendte'
-  },
-  {
-    label: 'Til Brænding',
-    to: '/bestillinger?status=tilbraending'
-  },
-  {
-    label: 'Brændt',
-    to: '/bestillinger?status=braendte'
-  },
-  {
-    label: 'Kontrolleret',
-    to: '/bestillinger?status=kontrolleret'
-  },
-  {
-    label: 'Direkte forsendelse',
-    to: '/bestillinger?status=direkteforsendelse'
-  }
-])
 </script>
 
 <template>
   <div class="card">
     <TabMenu :model="items" @tab-change="onTabChange" />
-    <router-view />
+    <!-- <router-view /> -->
   </div>
 </template>
 
